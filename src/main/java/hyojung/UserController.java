29e965c755e5d23301346fae46fa688066fd9a03
kimhyo009 <kimhyojung09@gmail.com>
2023-02-27 09:17:44 +0900

    0227 hyojung not yet

--- src/main/java/hyojung/UserController.java
+++ src/main/java/hyojung/UserController.java
@@ -37,6 +37,7 @@ public class UserController extends HttpServlet {
 		String page = "/hyojung/Login.jsp"; 
 		//다음에 내가 가야할 곳으로 보냄
 		String action = request.getPathInfo();
+		System.out.println("action : " + action);
 		// /user/* 상단 들어온 주소 값을 결정함? '/*' 해당하는 action값
 		
 		//회원가입
@@ -49,25 +50,23 @@ public class UserController extends HttpServlet {
 			String email = request.getParameter("email");
 			System.out.println(id+"/"+pwd1+"/"+name+"/"+email);
 			
-			if(pwd1.equals(pwd2)) {
-				UserService actSign = new UserService();
-				UserVO m = new UserVO();
-				m.setId(id);
-				m.setPwd(pwd1);
-				m.setName(name);
-				m.setEmail(email);
-				actSign.serSign(m);
+				if(pwd1.equals(pwd2)) {
+					UserService actSign = new UserService();
+					UserVO m = new UserVO();
+					m.setId(id);
+					m.setPwd(pwd1);
+					m.setName(name);
+					m.setEmail(email);
+					actSign.serSign(m);
+	
+					page ="/hyojung/LogIn.jsp";
+				} else {
+	//				request.setAttribute("message:", value);
+					page ="/hyojung/SignUp.jsp";
+				}
 
-				page ="/hyojung/LogIn.jsp";
-			} else {
-//				request.setAttribute("message:", value);
-				page ="/hyojung/SignUp.jsp";
-				
-			}
-
-			
+		//회원 중복확인
 		}else if(action.equals("/check")) {
-			//회원 중복확인
 			String id = (String) request.getParameter("id");
 			System.out.println("id = " + id);
 			UserService nameCheck = new UserService();
@@ -80,6 +79,7 @@ public class UserController extends HttpServlet {
 				} else {
 					writer.print("usable");
 				}
+				
 		//회원리스트 생성
 		} else if (action.equals("/userlist")) {
 			UserService actList = new UserService();
@@ -89,36 +89,81 @@ public class UserController extends HttpServlet {
 //<%-- !!!!!!!!!!!!!!파일 이동 시 변경해야 하는 주소!!!!!!!!!!!!!!!--%>
 			page ="/hyojung/LogIn.jsp";
 			
-			//로그인
+		//로그인
 		}else if (action.equals("/login")) {
+				HttpSession session = request.getSession();
+				System.out.println("session:" +session.getAttribute("id"));
+				String id = request.getParameter("id");
+				String pwd = request.getParameter("pwd");
+				System.out.println(id + pwd);
+				
+				UserService servlog = new UserService();
+				boolean result = servlog.servlog(id, pwd);
+				System.out.println("return되어 최종 돌려받은 값:"+result);
+				System.out.println(id+pwd);
+				
+				if( result ) {
+						session.setAttribute("id",id);
+	//<%-- !!!!!!!!!!!!!!파일 이동 시 변경해야 하는 주소!!!!!!!!!!!!!!!--%>
+						//창순씨 메인페이지 주소로 이동해야함.
+						page = "/hyojung/Mypage.jsp";
+						//
+						System.out.println("session:"+session);
+	
+				} else {
+	//<%-- !!!!!!!!!!!!!!파일 이동 시 변경해야 하는 주소!!!!!!!!!!!!!!!--%>
+					page = "/hyojung/LogIn.jsp";
+					System.out.println("로그인 실패");
+				}
+				System.out.println("로그인 출력");
+			
+		//마이페이지 내 정보 출력
+		} else if (action.equals("/mypage")){
 			HttpSession session = request.getSession();
-			System.out.println("session:" +session.getAttribute("id"));
-			String id = request.getParameter("id");
-			String pwd = request.getParameter("pwd");
-			System.out.println(id + pwd);
-			
-			UserService servlog = new UserService();
-			boolean result = servlog.servlog(id, pwd);
-			System.out.println("return되어 최종 돌려받은 값:"+result);
-			System.out.println(id+pwd);
-			
-			if( result ) {
-					session.setAttribute("id",id);
-//<%-- !!!!!!!!!!!!!!파일 이동 시 변경해야 하는 주소!!!!!!!!!!!!!!!--%>
-					page = "/hyojung/Mypage.jsp";
-					System.out.println("session:"+session);
-
-			} else {
-//<%-- !!!!!!!!!!!!!!파일 이동 시 변경해야 하는 주소!!!!!!!!!!!!!!!--%>
-				page = "/hyojung/LogIn.jsp";
-				System.out.println("로그인 실패");
-			}
-			System.out.println("로그인 출력");
+			String id = (String)session.getAttribute("id");
+			System.out.println("session id값:" +id);
+					if(id!=null) {
+						UserService mypage = new UserService();
+						UserVO result = mypage.serPage(id);
+						request.setAttribute("result", result);
+						System.out.println(result);					
+						
+						page="/hyojung/Mypage.jsp";
+					} else {
+						System.out.println("session id값이 없습니다.");
+						page="/hyojung/LogIn.jsp";
+					}
 		
-		//회원탈퇴
-// 		}else if(action) {
+		//마이페이지 내 정보 수정
+		} else if (action.equals("/update")) {
+			System.out.println("회원정보수정 출력");
+		
+			UserService actupdate = new UserService();
+			String id = request.getParameter("id");
+			String pwd1 = request.getParameter("pwd1");
+			String name = request.getParameter("name");
+			String email = request.getParameter("email");
+			System.out.println("update getParam:"+id+"/"+pwd1+"/"+"/"+name+"/"+email);
 			
-		}
+			String pwd2 = null;
+			if(pwd1.equals(pwd2)) {
+				UserVO vo = new UserVO();
+				vo.setId(id);
+				vo.setPwd(pwd1);
+				vo.setName(name);
+				vo.setEmail(email);
+				actupdate.serUpdate(vo);
+				page="/hyojung/Mypage.jsp";
+				}
+				
+		//회원탈퇴
+ 		}else if(action.equals("/delete")){
+ 				String delid = request.getParameter("id");
+				//delid.serDel();
+				page="/hyojung/Mypage.jsp";
+			
+ 		}
+	
 		
 		if(!action.equals("/check")) {
 			RequestDispatcher dispatch = request.getRequestDispatcher(page);
@@ -126,6 +171,28 @@ public class UserController extends HttpServlet {
 		}
 		
 
+//창순씨 로그인 구현
+////	로그인 세션
+//	HttpSession session = request.getSession();
+////	request.getParameter("")에 id 전달하는 name값
+//	String id = request.getParameter("");
+////	request.getParameter("")에 pwd 전달하는 name값
+//	String pwd = request.getParameter("");
+//	
+//	if(session.isNew()) {
+//		if(id != null) {
+//			session.setAttribute("id", pwd);
+//		} else {
+//			session.invalidate();
+//		}
+//	} else {
+//		id = (String) session.getAttribute("");
+//		if(id != null && id.length() != 0) {
+//			System.out.println("로그인성공");
+//		} else {
+//			session.invalidate();
+//		}
+//	}
 
 		
 		
--- src/main/java/hyojung/UserService.java
+++ src/main/java/hyojung/UserService.java
@@ -9,7 +9,6 @@ public class UserService {
 		System.out.println("serSigin이 VO를 다녀왔습니다");
 		UserDAO actSigin = new UserDAO();
 		 actSigin.addUser(m);
-
 	}
 	
 	//중복체크
@@ -17,7 +16,6 @@ public class UserService {
 		System.out.println("setCheck가 VO를 다녀왔습니다");
 		UserDAO setCheck = new UserDAO();
 		boolean result = setCheck.overlappedID(id);
-		
 		return result;
 	}
 	
@@ -26,7 +24,6 @@ public class UserService {
 		System.out.println("serList가 VO를 다녀왔습니다");
 		UserDAO addlist = new UserDAO();
 		List<UserVO>  list = addlist.listUser();
-		
 		return list;
 	}
 	//로그인
@@ -34,15 +31,30 @@ public class UserService {
 		System.out.println("id, pwd 값을 들고 왔는지 확인");
 		UserDAO actlogin = new UserDAO();
 		boolean result = actlogin.loginDAO(id, pwd);
+		return result;
+	}
+	
+	//마이페이지 내 정보 출력
+	public UserVO serPage(String id) {
+		System.out.println("serPage이 VO를 다녀왔습니다");
+		UserDAO actpage= new UserDAO();
+		UserVO result = actpage.mypage(id);
 		
 		return result;
 	}
-	//마이페이지
-//		public void serPage(String pwd, String name, String email) {
-//			System.out.println("serPage이 VO를 다녀왔습니다");
-//			MypageDAO actpage= new MypageDAO();
-//			String result = actpage.
-//		}
-
+	
+	//내 정보 업데이트
+	public void serUpdate(UserVO userVO) {
+		System.out.println("serUpdate가 VO를 다녀왔습니다");
+		UserDAO actupdate = new UserDAO();
+		actupdate.update(userVO);
+	}
+	
+	//회원삭제
+	public void serDel(String id) {
+		System.out.println("serDel이 VO를 다녀왔습니다");
+		UserDAO actdel= new UserDAO();
+		actdel.delete(id);
+	}
 	
 }
